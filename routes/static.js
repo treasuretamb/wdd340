77f075ab1fb73599cbaa23eb360caa47a5007f07
Treasure Tamb <treasuretamb@gmail.com>
2025-01-22 03:53:10 +0200

    assign website buid

--- routes/static.js
+++ routes/static.js
@@ -4,9 +4,12 @@ const router = express.Router();
 // Static Routes
 // Set up "public" folder / subfolders for static files
 router.use(express.static("public"));
-router.use("/css", express.static(__dirname + "public/css"));
-router.use("/js", express.static(__dirname + "public/js"));
-router.use("/images", express.static(__dirname + "public/images"));
+
+// Define routes
+router.get("/", (req, res) => {
+  res.render("index", { title: "Home Page" });
+});
+
 
 module.exports = router;
 
--- server.js
+++ server.js
@@ -5,26 +5,41 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+const express = require("express");
+const env = require("dotenv").config();
+const app = express();
+const static = require("./routes/static");
+const path = require("path");
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
 
+// Set ports
+const HOST = "localhost";
+const PORT = 5500;
+
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 5500;
+const host = process.env.HOST || 'localhost';
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
+app.use(express.static('public'));
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
+app.get('/', (req, res) => {
+  res.render('index', { content: `<h1>Treasure Tamb</h1><p>Welcome to the portfolio!</p>` });
+});
+
